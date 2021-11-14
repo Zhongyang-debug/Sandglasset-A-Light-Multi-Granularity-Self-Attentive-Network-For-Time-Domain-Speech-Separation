f5ed3c222458fe928ce5dfd8848ea8d579004206
Zhongyang <1147657495@qq.com>
2021-11-14 20:19:33 +0800

    code

---
+++ model/sandglasset.py
@@ -0,0 +1,559 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, out_channels):
+
+        super(Encoder, self).__init__()
+
+        self.Conv1d = nn.Conv1d(in_channels=1,
+                                out_channels=out_channels,
+                                kernel_size=2,
+                                stride=1,
+                                padding=0)
+
+        self.ReLU = nn.ReLU()
+
+    def forward(self, x):
+
+        x = torch.unsqueeze(x, dim=1)  # torch.Size([1, 32000]) => torch.Size([1, 1, 32000])
+
+        x = self.Conv1d(x)  # torch.Size([1, 1, 32000]) => torch.Size([1, 64, 31999])
+
+        x = self.ReLU(x)
+
+        return x
+
+
+class Segmentation(nn.Module):
+
+    def __init__(self, K):
+
+        super(Segmentation, self).__init__()
+
+        self.K = K
+
+    def forward(self, x):
+        """
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+           output: [B, N, K, S]
+        """
+
+        B, D, I = x.shape  # torch.Size([1, 128, 31999])
+
+        P = self.K // 2  # 100 = 200 // 2
+
+        input, gap = self._padding(x)  # 填充, torch.Size([1, 128, 32300]), 101
+
+        del x
+
+        input1 = input[:, :, :-P].contiguous().view(B, D, -1, self.K)  # torch.Size([1, 128, 161, 200])
+        input2 = input[:, :, P:].contiguous().view(B, D, -1, self.K)  # torch.Size([1, 128, 161, 200])
+
+        input = torch.cat([input1, input2], dim=3).view(B, D, -1, self.K).transpose(2, 3)  # torch.Size([1, 128, 200, 322])
+
+        return input.contiguous(), gap
+
+    def _padding(self, x):
+        """
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+        """
+        B, N, L = x.shape  # torch.Size([1, 128, 31999])
+
+        P = self.K // 2
+
+        gap = self.K - (P + L % self.K) % self.K  # 200 - (100 + 31999 % 200) % 200 = 101
+
+        if gap > 0:
+            pad = torch.Tensor(torch.zeros(B, N, gap)).type(x.type())  # torch.Size([1, 128, 101])
+            x = torch.cat([x, pad], dim=2)  # torch.Size([1, 128, 32100])
+
+        _pad = torch.Tensor(torch.zeros(B, N, P)).type(x.type())  # torch.Size([1, 128, 100])
+
+        x = torch.cat([_pad, x, _pad], dim=2)  # torch.Size([1, 128, 32300])
+
+        return x, gap
+
+
+class Locally_Recurrent(nn.Module):
+    """
+        LSTM
+        优点：改善了 RNN 中存在的长期依赖问题；LSTM 的表现通常比时间递归神经网络及隐马尔科夫模型（HMM）更好；作为非线性模型，LSTM 可作为复杂
+             的非线性单元用于构造更大型深度神经网络。
+        缺点：一个缺点是 RNN 的梯度问题在 LSTM 及其变种里面得到了一定程度的解决，但还是不够。它可以处理 100 个量级的序列，而对于 1000 个量级，
+             或者更长的序列则依然会显得很棘手；另一个缺点是每一个 LSTM 的 cell 里面都意味着有 4 个全连接层(MLP)，如果 LSTM 的时间跨度很大，
+             并且网络又很深，这个计算量会很大，很耗时。
+    """
+
+    def __init__(self, in_channels, hidden_channels=128, num_layers=1, bidirectional=True):
+
+        super(Locally_Recurrent, self).__init__()
+
+        """
+            sequence: 序列的个数，每个句子的长度；
+            batch_size：输入模型多少个句子，或者股票数据输入模型多少个时间单位的数据；
+            feature：每个具体的单词用多少维向量表示，或者股票数据中每一个具体时刻采集多少个具体的值。
+        """
+        self.Bi_LSTM = nn.LSTM(input_size=in_channels,  # 输入的维度
+                               hidden_size=hidden_channels,  # 隐藏层的维度
+                               num_layers=num_layers,  # LSTM 的层数
+                               bias=True,  # 偏置
+                               batch_first=True,  # True, (batch, seq, feature); False, (seq, batch, feature)
+                               bidirectional=bidirectional)  # 是否为双向 LSTM
+
+        self.LayerNorm = nn.LayerNorm(normalized_shape=in_channels)
+
+        # 线性层
+        self.Linear = nn.Linear(hidden_channels*2 if bidirectional else hidden_channels, in_channels)
+
+    def forward(self, x):
+
+        B, N, K, S = x.shape  # torch.Size([1, 128, 200, 322])
+
+        x = x.permute(0, 3, 2, 1).reshape(B*S, K, N)  # 调整维度通过 LSTM, torch.Size([322, 200, 128])
+
+        residual1 = x  # 残余连接
+
+        x, _ = self.Bi_LSTM(x)  # 双向 LSTM, torch.Size([200, 322, 256])
+
+        x = self.Linear(x)  # 线性层减少输出通道, torch.Size([200, 322, 128])
+
+        x = self.LayerNorm(x + residual1)  # 层归一化
+
+        del residual1
+
+        x = x.reshape(B, S, K, N).permute(0, 3, 2, 1)  # 恢复原来的维度, torch.Size([1, 128, 200, 322])
+
+        return x
+
+
+class Positional_Encoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+
+        super(Positional_Encoding, self).__init__()
+
+        # 从理解来讲，需要注意的就是偶数和奇数在公式上有一个共同部分，我们使用 log 函数把次方拿下来，方便计算；
+        # pos 代表的是单词在句子中的索引，这点需要注意；比如 max_len 是 128 个，那么索引就是从 0, 1, 2,..., 127
+        # 假设我的 d_model 是 512, 2i 那个符号中 i 从 0 取到了 255，那么 2i 对应取值就是 0, 2, 4, ..., 510
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)  # pe[:, 0::2]是从 0 开始到最后面，补长为 2，其实代表的就是偶数位置
+        pe[:, 1::2] = torch.cos(position * div_term)  # pe[:, 1::2]是从 1 开始到最后面，补长为 2，其实代表的就是奇数位置
+        # 上面代码获取之后得到的 pe: [max_len*d_model]
+
+        pe = pe.unsqueeze(0).transpose(0, 1)  # [max_len*1*d_model]
+
+        self.register_buffer('pe', pe)  # 定一个缓冲区，其实简单理解为这个参数不更新就可以
+
+    def forward(self, x):
+
+        """
+            x: [seq_len, batch_size, d_model]
+        """
+
+        x = x + self.pe[:x.size(0), :]
+
+        return self.dropout(x)
+
+
+class Globally_Attentive(nn.Module):
+
+    def __init__(self, in_channels, num_heads=8):
+
+        super(Globally_Attentive, self).__init__()
+
+        self.LayerNorm1 = nn.LayerNorm(normalized_shape=in_channels)
+
+        self.Positional_Encoding = Positional_Encoding(d_model=in_channels,
+                                                       dropout=0.1,
+                                                       max_len=8000)
+
+        self.MultiheadAttention = nn.MultiheadAttention(embed_dim=in_channels,
+                                                        num_heads=num_heads,
+                                                        dropout=0.1)
+
+        self.LayerNorm2 = nn.LayerNorm(normalized_shape=in_channels)
+
+        self.Dropout = nn.Dropout(p=0.1)
+
+    def forward(self, x):
+
+        B, N, K, S = x.shape  # torch.Size([1, 128, 200, 322])
+
+        residual1 = x  # 残余连接, torch.Size([1, 128, 200, 322])
+
+        x = x.permute(0, 2, 3, 1).reshape(B*K, S, N)  # 调整维度, 注意与循环网络区分，分别为局部和全局, torch.Size([200, 322, 128])
+
+        x = self.LayerNorm1(x) + self.Positional_Encoding(x)  # 加入位置信息, torch.Size([200, 322, 128])
+
+        residual2 = x
+
+        x = self.MultiheadAttention(x, x, x, attn_mask=None, key_padding_mask=None)[0]  # torch.Size([200, 322, 128])
+
+        x = residual2 + self.Dropout(x)  # torch.Size([200, 322, 128])
+
+        del residual2
+
+        x = self.LayerNorm2(x)
+
+        x = x.reshape(B, K, S, N).permute(0, 3, 1, 2)  # torch.Size([1, 128, 200, 322])
+
+        x = x + residual1  # torch.Size([1, 128, 200, 322])
+
+        del residual1
+
+        return x
+
+
+class Sandglasset_Block(nn.Module):
+
+    def __init__(self, in_channels, hidden_channels=128, num_layers=1, bidirectional=True, num_heads=8,
+                 kernel_size=1, stride=1):
+
+        super(Sandglasset_Block, self).__init__()
+
+        self.Locally_Recurrent = Locally_Recurrent(in_channels=in_channels,
+                                                   hidden_channels=hidden_channels,
+                                                   num_layers=num_layers,
+                                                   bidirectional=bidirectional)
+
+        self.Conv1D = nn.Conv1d(in_channels=in_channels,
+                                out_channels=in_channels,
+                                kernel_size=kernel_size,
+                                stride=stride,
+                                padding=0)
+
+        self.Globally_Attentive = Globally_Attentive(in_channels=in_channels,
+                                                     num_heads=num_heads)
+
+        self.ConvTrans1D = nn.ConvTranspose1d(in_channels=in_channels,
+                                              out_channels=in_channels,
+                                              kernel_size=kernel_size,
+                                              stride=stride,
+                                              padding=0)
+
+    def forward(self, x):
+
+        B, N, K, S = x.shape  # torch.Size([1, 64, 200, 322])
+
+        x = self.Locally_Recurrent(x)  # torch.Size([1, 64, 200, 322])
+
+        x = x.permute(0, 3, 1, 2).reshape(B*S, N, K)
+
+        x = self.Conv1D(x)
+
+        x = x.reshape(B, S, N, -1).permute(0, 2, 3, 1)
+
+        x = self.Globally_Attentive(x)
+
+        x = x.permute(0, 3, 1, 2).reshape(B*S, N, -1)
+
+        x = self.ConvTrans1D(x)
+
+        x = x.reshape(B, S, N, -1).permute(0, 2, 3, 1)
+
+        return x
+
+
+class Separation(nn.Module):
+
+    def __init__(self, in_channels, out_channels, length, hidden_channels=128, num_layers=1, bidirectional=True,
+                 num_heads=8, cycle_amount=6, speakers=2):
+
+        super(Separation, self).__init__()
+
+        self.LayerNorm = nn.LayerNorm(normalized_shape=in_channels)  # 128
+
+        self.Conv1d = nn.Conv1d(in_channels=in_channels,  # 128
+                                out_channels=out_channels,  # 64
+                                kernel_size=1)
+
+        self.Segmentation = Segmentation(K=length)
+
+        self.cycle_amount = cycle_amount
+
+        kernel_size = []
+        stride = []
+
+        for i in range(self.cycle_amount):
+            if i < self.cycle_amount//2:
+                kernel_size.append(4**i)
+                stride.append(4**i)
+            else:
+                kernel_size.append(4**(self.cycle_amount-i-1))
+                stride.append(4**(self.cycle_amount-i-1))
+
+        self.Sandglasset_Blocks = nn.ModuleList([])
+
+        for i in range(self.cycle_amount):
+            self.Sandglasset_Blocks.append(Sandglasset_Block(in_channels=out_channels,
+                                                             hidden_channels=hidden_channels,
+                                                             num_layers=num_layers,
+                                                             bidirectional=bidirectional,
+                                                             num_heads=num_heads,
+                                                             kernel_size=kernel_size[i],
+                                                             stride=stride[i]))
+
+        self.residual = []
+
+        self.PReLU = nn.PReLU()
+
+        self.Spk = speakers
+
+        self.Conv2d = nn.Conv2d(in_channels=out_channels,  # 64
+                                out_channels=speakers*out_channels,  # 128
+                                kernel_size=1)
+
+        self.output = nn.Sequential(nn.Conv1d(in_channels=out_channels,  # 64
+                                              out_channels=out_channels,   # 64
+                                              kernel_size=1),  # 64
+                                    nn.Tanh())
+
+        self.output_gate = nn.Sequential(nn.Conv1d(in_channels=out_channels,  # 64
+                                                   out_channels=out_channels,  # 64
+                                                   kernel_size=1),
+                                         nn.Sigmoid())
+
+        self.Conv1x1 = nn.Conv1d(in_channels=out_channels,  # 64
+                                 out_channels=in_channels,  # 128
+                                 kernel_size=1,
+                                 bias=False)
+
+        self.ReLU = nn.ReLU()
+
+    def forward(self, x):
+
+        # torch.Size([1, 128, 31999]) => torch.Size([1, 31999, 128])
+        x = x.permute(0, 2, 1)  # torch.Size([1, 31999, 128])
+
+        # 层归一化
+        x = self.LayerNorm(x)  # torch.Size([1, 31999, 128])
+
+        # torch.Size([1, 31999, 128]) => torch.Size([1, 128, 31999])
+        x = x.permute(0, 2, 1)  # torch.Size([1, 128, 31999])
+
+        # 一维卷积
+        # torch.Size([1, 128, 31999]) => torch.Size([1, 64, 31999])
+        x = self.Conv1d(x)  # torch.Size([1, 64, 31999])
+
+        x, gap = self.Segmentation(x)  # torch.Size([1, 64, 31999]) => torch.Size([1, 64, 200, 322])
+
+        self.residual = []
+
+        for i in range(self.cycle_amount):
+
+            x = self.Sandglasset_Blocks[i](x)  # torch.Size([1, 64, 200, 322])
+
+            if i < self.cycle_amount//2:
+                self.residual.append(x)
+            else:
+                x = x + self.residual[self.cycle_amount-i-1]
+
+        x = self.PReLU(x)
+
+        x = self.Conv2d(x)  # 通道说话人个数倍, torch.Size([1, 128, 200, 322])
+
+        B, _, K, S = x.shape
+
+        x = x.reshape(self.Spk, -1, K, S)  # 调整torch.Size([2, 64, 200, 322])
+
+        x = self._over_add(x, gap)  # torch.Size([2, 64, 31999])
+
+        x = self.output(x) * self.output_gate(x)  # torch.Size([2, 64, 31999])
+
+        x = self.Conv1x1(x)  # torch.Size([2, 128, 31999])
+
+        _, N, L = x.shape
+
+        x = x.reshape(B, self.Spk, N, L)  # torch.Size([1, 2, 128, 31999])
+
+        x = self.ReLU(x)
+
+        x = x.transpose(0, 1)  # torch.Size([2, 1, 128, 31999])
+
+        return x
+
+    def _over_add(self, input, gap):
+        """
+           input: [B, N, K, S]
+           gap: padding length
+           output: [B, N, L]
+        """
+        B, N, K, S = input.shape
+        P = K // 2
+
+        # [B, N, S, K]
+        input = input.transpose(2, 3).contiguous().view(B, N, -1, K * 2)
+
+        input1 = input[:, :, :, :K].contiguous().view(B, N, -1)[:, :, P:]
+        input2 = input[:, :, :, K:].contiguous().view(B, N, -1)[:, :, :-P]
+        input = input1 + input2
+
+        # [B, N, L]
+        if gap > 0:
+            input = input[:, :, :-gap]
+
+        return input
+
+
+class Decoder(nn.ConvTranspose1d):
+    """
+        Decoder of the TasNet
+        This module can be seen as the gradient of Conv1d with respect to its input.
+        It is also known as a fractionally-strided convolution
+        or a deconvolution (although it is not an actual deconvolution operation).
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Decoder, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        """
+            x: [B, N, L]
+        """
+
+        if x.dim() not in [2, 3]:
+            raise RuntimeError("{} accept 3/4D tensor as input".format(self.__name__))
+
+        x = super().forward(x if x.dim() == 3 else torch.unsqueeze(x, 1))
+
+        if torch.squeeze(x).dim() == 1:
+            x = torch.squeeze(x, dim=1)
+        else:
+            x = torch.squeeze(x)
+
+        return x
+
+
+class Sandglasset(nn.Module):
+
+    def __init__(self, in_channels, out_channels, length, hidden_channels=128, num_layers=1, bidirectional=True, num_heads=8, cycle_amount=6,
+                 speakers=2):
+
+        super(Sandglasset, self).__init__()
+
+        self.in_channels = in_channels
+
+        self.out_channels = out_channels
+
+        self.length = length
+
+        self.hidden_channels = hidden_channels
+
+        self.num_layers = num_layers
+
+        self.bidirectional = bidirectional
+
+        self.num_heads = num_heads
+
+        self.cycle_amount = cycle_amount
+
+        self.speakers = speakers
+
+        self.Encoder = Encoder(out_channels=self.in_channels)
+
+        self.Separation = Separation(in_channels=self.in_channels,
+                                     out_channels=self.out_channels,
+                                     length=self.length,
+                                     hidden_channels=self.hidden_channels,
+                                     num_layers=self.num_layers,
+                                     bidirectional=self.bidirectional,
+                                     num_heads=self.num_heads,
+                                     cycle_amount=self.cycle_amount,
+                                     speakers=self.speakers)
+
+        self.Spk = self.speakers
+
+        self.Decoder = Decoder(in_channels=self.in_channels,
+                               out_channels=1,
+                               kernel_size=2,
+                               stride=1,
+                               padding=0,
+                               bias=False)
+
+    def forward(self, x):
+
+        e = self.Encoder(x)  # torch.Size([1, 32000]) => torch.Size([1, 128, 31999])
+
+        s = self.Separation(e)  # torch.Size([2, 1, 128, 31999])
+
+        out = [s[i]*e for i in range(self.Spk)]
+
+        del e, s
+
+        audio = [self.Decoder(out[i]) for i in range(self.Spk)]
+
+        del out
+
+        audio = torch.stack(audio, dim=1)
+
+        return audio
+
+    @classmethod
+    def load_model(cls, path):
+        package = torch.load(path, map_location=lambda storage, loc: storage)
+        model = cls.load_model_from_package(package)
+        return model
+
+    @classmethod
+    def load_model_from_package(cls, package):
+        model = cls(in_channels=package['in_channels'], out_channels=package['out_channels'],
+                    length=package['length'], hidden_channels=package['hidden_channels'],
+                    num_layers=package['num_layers'], bidirectional=package['bidirectional'],
+                    num_heads=package['num_heads'], cycle_amount=package['cycle_amount'],
+                    speakers=package['speakers'])
+        model.load_state_dict(package['state_dict'])
+        return model
+
+    @staticmethod
+    def serialize(model, optimizer, epoch, tr_loss=None, cv_loss=None):
+        package = {
+            # hyper-parameter
+            'in_channels': model.in_channels, 'out_channels': model.out_channels,
+            'length': model.length, 'hidden_channels': model.hidden_channels,
+            'num_layers': model.num_layers, 'bidirectional': model.bidirectional,
+            'num_heads': model.num_heads, 'cycle_amount': model.cycle_amount,
+            'speakers': model.speakers,
+
+            # state
+            'state_dict': model.state_dict(),
+            'optim_dict': optimizer.state_dict(),
+            'epoch': epoch
+        }
+
+        if tr_loss is not None:
+            package['tr_loss'] = tr_loss
+            package['cv_loss'] = cv_loss
+        return package
+
+
+if __name__ == "__main__":
+
+    x = torch.rand(1, 32000)
+
+    model = Sandglasset(in_channels=64,
+                        out_channels=64,
+                        length=256,
+                        hidden_channels=128,
+                        num_layers=1,
+                        bidirectional=True,
+                        num_heads=8,
+                        cycle_amount=4,
+                        speakers=2)
+
+    y = model(x)
+
+    print("{:.3f} million".format(sum([param.nelement() for param in model.parameters()]) / 1e6))
+
+    print(y.shape)
---
+++ train.py
@@ -0,0 +1,111 @@
+import argparse
+import torch
+from dataset.data import AudioDataLoader, AudioDataset
+from src.trainer import Trainer
+from model.conv_tasnet import ConvTasNet
+from model.dual_path_rnn import Dual_RNN_model
+from model.dptnet import DPTNet
+from model.sepformer import Sepformer
+from model.sudormrf import SuDORMRF
+from model.galr import GALR
+from model.sandglasset import Sandglasset
+import json5
+import numpy as np
+from adamp import AdamP, SGDP
+
+
+def main(config):
+    torch.manual_seed(config["seed"])
+    np.random.seed(config["seed"])
+
+    # 数据
+    tr_dataset = AudioDataset(json_dir=config["train_dataset"]["train_dir"],  # 目录下包含 mix.json, s1.json, s2.json
+                              batch_size=config["train_dataset"]["batch_size"],
+                              sample_rate=config["train_dataset"]["sample_rate"],  # 采样率
+                              segment=config["train_dataset"]["segment"])  # 语音时长
+
+    cv_dataset = AudioDataset(json_dir=config["validation_dataset"]["validation_dir"],
+                              batch_size=config["validation_dataset"]["batch_size"],
+                              sample_rate=config["validation_dataset"]["sample_rate"],
+                              segment=config["validation_dataset"]["segment"],
+                              cv_max_len=config["validation_dataset"]["cv_max_len"])
+
+    tr_loader = AudioDataLoader(tr_dataset,
+                                batch_size=config["train_loader"]["batch_size"],
+                                shuffle=config["train_loader"]["shuffle"],
+                                num_workers=config["train_loader"]["num_workers"])
+
+    cv_loader = AudioDataLoader(cv_dataset,
+                                batch_size=config["validation_loader"]["batch_size"],
+                                shuffle=config["validation_loader"]["shuffle"],
+                                num_workers=config["validation_loader"]["num_workers"])
+
+    data = {"tr_loader": tr_loader, "cv_loader": cv_loader}
+
+    # 模型
+    if config["model"]["type"] == "sandglasset":
+        model = Sandglasset(in_channels=config["model"]["sandglasset"]["in_channels"],
+                            out_channels=config["model"]["sandglasset"]["out_channels"],
+                            length=config["model"]["sandglasset"]["length"],
+                            hidden_channels=config["model"]["sandglasset"]["hidden_channels"],
+                            num_layers=config["model"]["sandglasset"]["num_layers"],
+                            bidirectional=config["model"]["sandglasset"]["bidirectional"],
+                            num_heads=config["model"]["sandglasset"]["num_heads"],
+                            cycle_amount=config["model"]["sandglasset"]["cycle_amount"],
+                            speakers=config["model"]["sandglasset"]["speakers"])
+    else:
+        print("No loaded model!")
+
+    if torch.cuda.is_available():
+        model = torch.nn.DataParallel(model)
+        model.cuda()
+
+    if config["optimizer"]["type"] == "sgd":
+        optimize = torch.optim.SGD(
+            params=model.parameters(),
+            lr=config["optimizer"]["sgd"]["lr"],
+            momentum=config["optimizer"]["sgd"]["momentum"],
+            weight_decay=config["optimizer"]["sgd"]["l2"])
+    elif config["optimizer"]["type"] == "adam":
+        optimize = torch.optim.Adam(
+            params=model.parameters(),
+            lr=config["optimizer"]["adam"]["lr"],
+            betas=(config["optimizer"]["adam"]["beta1"], config["optimizer"]["adam"]["beta2"]))
+    elif config["optimizer"]["type"] == "sgdp":
+        optimize = SGDP(
+            params=model.parameters(),
+            lr=config["optimizer"]["sgdp"]["lr"],
+            weight_decay=config["optimizer"]["sgdp"]["weight_decay"],
+            momentum=config["optimizer"]["sgdp"]["momentum"],
+            nesterov=config["optimizer"]["sgdp"]["nesterov"],
+        )
+    elif config["optimizer"]["type"] == "adamp":
+        optimize = AdamP(
+            params=model.parameters(),
+            lr=config["optimizer"]["adamp"]["lr"],
+            betas=(config["optimizer"]["adamp"]["beta1"], config["optimizer"]["adamp"]["beta2"]),
+            weight_decay=config["optimizer"]["adamp"]["weight_decay"],
+        )
+    else:
+        print("Not support optimizer")
+        return
+
+    trainer = Trainer(data, model, optimize, config)
+
+    trainer.train()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Speech Separation")
+
+    parser.add_argument("-C",
+                        "--configuration",
+                        default="./config/train/train.json5",
+                        type=str,
+                        help="Configuration (*.json).")
+
+    args = parser.parse_args()
+
+    configuration = json5.load(open(args.configuration))
+
+    main(configuration)
