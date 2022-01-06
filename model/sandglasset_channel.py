fba2392722a7372a18fbef3c8286f25f12b305f5
zydl <68770882+Zhongyang-debug@users.noreply.github.com>
2022-01-06 20:14:43 +0800

    Update sandglasset_channel.py

--- model/sandglasset_channel.py
+++ model/sandglasset_channel.py
@@ -150,6 +150,31 @@ class Positional_Encoding(nn.Module):
         return self.pe[:, :length]
 
 
+class Self_Attention(nn.Module):
+    # input : batch_size * seq_len * input_dim
+    # q : batch_size * input_dim * dim_k
+    # k : batch_size * input_dim * dim_k
+    # v : batch_size * input_dim * dim_v
+    def __init__(self, input_dim, dim_k, dim_v):
+        super(Self_Attention, self).__init__()
+        self.q = nn.Linear(input_dim, dim_k)
+        self.k = nn.Linear(input_dim, dim_k)
+        self.v = nn.Linear(input_dim, dim_v)
+        self._norm_fact = 1 / sqrt(dim_k)
+
+    def forward(self, x):
+        Q = self.q(x)  # Q: batch_size * seq_len * dim_k
+        K = self.k(x)  # K: batch_size * seq_len * dim_k
+        V = self.v(x)  # V: batch_size * seq_len * dim_v
+
+        atten = nn.Softmax(dim=-1)(
+            torch.bmm(Q, K.permute(0, 2, 1))) * self._norm_fact  # Q * K.T() # batch_size * seq_len * seq_len
+
+        output = torch.bmm(atten, V)  # Q * K.T() * V # batch_size * seq_len * dim_v
+
+        return output
+
+
 class Globally_Attentive(nn.Module):
 
     def __init__(self, in_channels, num_heads=8):
@@ -161,9 +186,7 @@ class Globally_Attentive(nn.Module):
         self.Positional_Encoding = Positional_Encoding(d_model=in_channels,
                                                        max_len=8000)
 
-        self.MultiheadAttention = nn.MultiheadAttention(embed_dim=in_channels,
-                                                        num_heads=num_heads,
-                                                        dropout=0.1)
+        self.Self_Attention = Self_Attention(input_dim=in_channels, dim_k=in_channels, dim_v=in_channels)
 
         self.LayerNorm2 = nn.LayerNorm(normalized_shape=in_channels)
 
@@ -181,7 +204,7 @@ class Globally_Attentive(nn.Module):
 
         residual2 = x
 
-        x = self.MultiheadAttention(x, x, x, attn_mask=None, key_padding_mask=None)[0]  # torch.Size([200, 322, 128])
+        x = self.Self_Attention(x)  # torch.Size([200, 322, 128])
 
         x = residual2 + self.Dropout(x)  # torch.Size([200, 322, 128])
 
